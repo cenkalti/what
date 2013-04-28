10a26d6b852bac3c31bdea23a157b7eb72924148
Cenk Alti <cenkalti@gmail.com>
2013-04-28 09:17:12 +0300

    bump version

--- what/__init__.py
+++ what/__init__.py
@@ -6,7 +6,7 @@ from subprocess import Popen, PIPE, STDOUT
 from ringbuffer import RingBuffer
 from exceptions import Timeout, EOF, UnexpectedExit
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 
 class What(Popen):
