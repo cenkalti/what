efd8983f2fbe6c65619abd2fb090c737ae79ca6a
Cenk Alti <cenkalti@gmail.com>
2013-09-01 09:46:59 +0300

    simplify test program

--- program.py
+++ program.py
@@ -2,30 +2,17 @@
 Sample program that is used in tests.
 
 """
-import sys
-from sys import argv, exit
+from sys import stdout, argv, exit
 from time import sleep
 
 
-class Unbuffered(object):
-    """http://stackoverflow.com/a/107717/242451"""
-
-    def __init__(self, stream):
-        self.stream = stream
-
-    def write(self, data):
-        self.stream.write(data)
-        self.stream.flush()
-
-    def __getattr__(self, attr):
-        return getattr(self.stream, attr)
-
-
 if __name__ == '__main__':
-    # make output unbuffered
-    sys.stdout = Unbuffered(sys.stdout)
-
-    print argv[1]
+    stdout.write(argv[1])
+    stdout.flush()
+    
     sleep(int(argv[2]))
-    print argv[3]
+    
+    stdout.write(argv[3])
+    stdout.flush()
+    
     exit(int(argv[4]))
